ea87f3e67dd402acecc7cd92d56ae0e8691c0954
fu92n3m2fm223 <rddragon223+@gmail.com>
2024-07-04 14:01:10 -0500

    Update SolaraFreedom.lua

--- Files/SolaraFreedom.lua
+++ Files/SolaraFreedom.lua
@@ -691,7 +691,6 @@ Cheats2:AddButton({
 				local Player = game:GetService("Players"):GetPlayerFromCharacter(Character)
 				Colossal = tostring("Colossal: " .. Player.Name)
 			end
-			task.wait()
 		end
 
 		if Female then
