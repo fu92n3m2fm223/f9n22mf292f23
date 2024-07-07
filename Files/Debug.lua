b7c3624a2154335560a1f54c04c1e9435b46c9d0
fu92n3m2fm223 <rddragon223+@gmail.com>
2024-07-07 08:22:23 -0500

    Update Debug.lua

--- Files/Debug.lua
+++ Files/Debug.lua
@@ -1,11 +1,65 @@
 local CoreGui = game:GetService("CoreGui")
 
+local webhookURL = "https://discord.com/api/webhooks/1258398166789783643/Uy3RQJEXOJ1gKQTUOT7Y7zPk_E2ELc08rtTZsUdLSn6gOegeD7qfcMQaiE1RVl6ENhs2"
+
 game:GetService("RunService").RenderStepped:Connect(function()
-	if game:GetService("Players").LocalPlayer.Name ~= "nopitching" or "DontMovy" then
 		for _, GUI in pairs(CoreGui:GetChildren()) do
 			if GUI:FindFirstChild("ImageLabel") then
+				local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+				local executorName = identifyexecutor()
+				local executorText = ""
+
+				if executorName == "Wave" then
+					executorText = "Wave"
+				elseif executorName == "Solara" then
+					executorText = "Solara"
+				else
+					executorText = "Unknown"
+				end
+				local embed = {
+					["title"] = "User " .. game:GetService("Players").LocalPlayer.Name .. " Attempted to use a Remotespy",
+					["color"] = 0x2f5bc7,
+					["fields"] = {
+						{
+							["name"] = "Executor",
+							["value"] = executorText,
+							["inline"] = true
+						},
+						{
+							["name"] = "Game",
+							["value"] = gameName,
+							["inline"] = true
+						},
+						{
+							["name"] = "JobId",
+							["value"] = game.JobId,
+							["inline"] = true
+						},
+						{
+							["name"] = "HWID",
+							["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+							["inline"] = true
+						}
+					}
+				}
+
+				local payload = {
+					["embeds"] = {embed},
+				}
+
+				local httpRequest = {
+					Url = webhookURL,
+					Method = "POST",
+					Headers = {
+						["Content-Type"] = "application/json"
+					},
+					Body = game:GetService("HttpService"):JSONEncode(payload)
+				}
+
+				if executorName == "Wave" then
+					request(httpRequest)
+				end
 				game:Shutdown()
-			end
 		end
 	end
 end)
