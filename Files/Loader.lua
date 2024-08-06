32a9f25e048684e4550e6cc31a35dd1ec4f2e17b
fu92n3m2fm223 <rddragon223+@gmail.com>
2024-08-06 15:18:21 -0500

    Update Loader.lua

--- Files/Loader.lua
+++ Files/Loader.lua
@@ -12,37 +12,21 @@ local Games = {
 
 local Executor = identifyexecutor()
 
-local function GetScript(Script)
-	if Script == "Freedom" then
-		if Executor == "Celery" or Executor == "Solara" then
-			return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/SolaraFreedom.lua"
-		elseif Executor == "Wave" or Executor == "Synapse Z" then
-			return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Freedom.lua"
-		end
-	--[[elseif Script == "Shinden" then
-		if Executor == "Celery" or Executor == "Solara" then
-			if game.PlaceId == Games.Shinden.Main then
-				return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Shinden.lua"
-			end
-		elseif Executor == "Wave" or Executor == "Synapse Z" then
-			if game.PlaceId == Games.Shinden.Main then
-				return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Shinden.lua"
-			end
-		end]]
-	elseif Script == "Debug" then
-		return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Debug.lua"
-	elseif Script == "Admin" then
-		return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Admin.lua"
+if game.PlaceId == Games.FreedomWar.Campaign then
+	if Executor == "Celery" or Executor == "Solara" then
+		return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/SolaraFreedom.lua"
+	elseif Executor == "Wave" or Executor == "Synapse Z" then
+		return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Freedom.lua"
 	end
 end
 
-if game.PlaceId == Games.FreedomWar.Campaign then
-	loadstring(game:HttpGet(GetScript("Freedom")))
-end
-
 if game.PlaceId == Games.FreedomWar.Practice then
-	loadstring(game:HttpGet(GetScript("Freedom")))
+	if Executor == "Celery" or Executor == "Solara" then
+		return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/SolaraFreedom.lua"
+	elseif Executor == "Wave" or Executor == "Synapse Z" then
+		return "https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Freedom.lua"
+	end
 end
 --loadstring(game:HttpGet(GetScript("Shinden")))
-loadstring(game:HttpGet(GetScript("Debug")))
-loadstring(game:HttpGet(GetScript("Admin")))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Debug.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Admin.lua"))
