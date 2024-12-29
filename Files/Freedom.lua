07d0e3e47d6362e5af80e488825060f0bd070fe7
fu92n3m2fm223 <rddragon223+@gmail.com>
2024-12-29 14:31:05 -0600

    Update Freedom.lua

--- Files/Freedom.lua
+++ Files/Freedom.lua
@@ -1687,9 +1687,23 @@ do
 	end)]]
 
 	local HorseStamina = Tabs.Sixth:AddToggle("HorseStamina", {Title = "Infinite Horse Stamina", Default = false, })
+	local HorseGod = Tabs.Sixth:AddToggle("HorseGod", {Title = "Horse God Mode", Default = false, })
 
 	HorseStamina:OnChanged(function()
 		getgenv().horsestam = Options.HorseStamina.Value
+	end)
+	
+	HorseGod:OnChanged(function()
+		getgenv().horsegod = Options.HorseGod.Value
+		if getgenv().horsegod == false then
+			for i,v in pairs(workspace:WaitForChild("OnGameHorses"):GetChildren()) do
+				if v:WaitForChild("Humanoid"):WaitForChild("Owner").Value == Player.Name then
+					if isnetworkowner(v:WaitForChild("HumanoidRootPart")) then
+						v:WaitForChild("Humanoid").Health = 50
+					end
+				end
+			end
+		end
 	end)
 
 	local HorseSpeed = Tabs.Sixth:AddSlider("HorseSpeedSlider", {
@@ -1859,13 +1873,16 @@ do
 						if horseOwner == Player.Name and horseHumanoid.Mounted.Value == true then
 							local config = horseHumanoid.Parent.Configuration
 
-							local god = horseHumanoid.God
 							local Stam = config.Stamina
 							local speed = config.CurrentSpeed
 							local maxspeed = config.MaxSpeed
 
-							if god and getgenv().horsegod then
-								god.Value = Options.HorseGod.Value
+							if horseHumanoid and getgenv().horsegod then
+								if isnetworkowner(horse:WaitForChild("HumanoidRootPart")) then
+									if horseHumanoid.Health ~= "nan" then
+										horseHumanoid.Health = "nan"
+									end
+								end
 							end
 
 							if Stam and getgenv().horsestam then
