b3cbe0ef9857905ae9df3677f9d5ce01e7460d33
fu92n3m2fm223 <rddragon223+@gmail.com>
2025-01-05 15:46:10 -0600

    Update Loader.lua

--- Files/Loader.lua
+++ Files/Loader.lua
@@ -12,6 +12,8 @@ local Games = {
 
 local Executor = identifyexecutor()
 
+print(AUTH_USER_DISCORD_ID)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Admin.lua"))()
 
 if game.PlaceId == Games.FreedomWar.Campaign then
