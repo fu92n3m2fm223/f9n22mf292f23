7252cfcc7b69800fefef598f2f0232822dc67cc8
fu92n3m2fm223 <rddragon223+@gmail.com>
2025-02-10 16:45:51 -0600

    Update Loader.lua

--- Files/Loader.lua
+++ Files/Loader.lua
@@ -15,7 +15,7 @@ local Executor = identifyexecutor()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Admin.lua"))()
 
 if game.PlaceId == Games.FreedomWar.Campaign then
-	if Executor == "Celery" or Executor == "Solara" or Executor == "Swift" or Executor == "Xeno" then
+	if Executor == "Celery" or Executor == "Solara" or Executor == "Swift" or Executor == "Sonar" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/SolaraFreedom.lua"))()
 	elseif Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "SirHurt" or Executor == "Sonar" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Freedom.lua"))()
@@ -23,15 +23,15 @@ if game.PlaceId == Games.FreedomWar.Campaign then
 end
 
 if game.PlaceId == Games.FreedomWar.Practice then
-	if Executor == "Celery" or Executor == "Solara" or Executor == "Swift" or Executor == "Xeno" then
+	if Executor == "Celery" or Executor == "Solara" or Executor == "Swift" or Executor == "Sonar" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/SolaraFreedom.lua"))()
-	elseif Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "SirHurt" or Executor == "Sonar" then
+	elseif Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "SirHurt" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Freedom.lua"))()
 	end
 end
 
 if game.PlaceId == Games.Shinden.Main then
-	if Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "Swift" or Executor == "Argon" then
+	if Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "Swift" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Shinden.lua"))()
 	end
 end
