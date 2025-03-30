d56908c562b2bbb223765f8d306d134114648291
fu92n3m2fm223 <rddragon223+@gmail.com>
2025-03-29 22:38:27 -0500

    Update Loader.lua

--- Files/Loader.lua
+++ Files/Loader.lua
@@ -17,7 +17,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf
 if game.PlaceId == Games.FreedomWar.Campaign then
 	if Executor == "Celery" or Executor == "Solara" or Executor == "Swift" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/SolaraFreedom.lua"))()
-	elseif Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "SirHurt" or Executor == "Velocity" or Executor == "Potassium" then
+	elseif Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "Potassium" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Freedom.lua"))()
 	end
 end
@@ -25,7 +25,7 @@ end
 if game.PlaceId == Games.FreedomWar.Practice then
 	if Executor == "Celery" or Executor == "Solara" or Executor == "Swift" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/SolaraFreedom.lua"))()
-	elseif Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "SirHurt" or Executor == "Velocity" or Executor == "Potassium" then
+	elseif Executor == "Wave" or Executor == "Synapse Z" or Executor == "Rebel" or Executor == "AWP" or Executor == "Seliware" or Executor == "Potassium" then
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/fu92n3m2fm223/f9n22mf292f23/main/Files/Freedom.lua"))()
 	end
 end
